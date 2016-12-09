d7eb5b788e7029c32a780055160dbb3c0dd14902
hpradyum@in.ibm.com <indulas1@in.ibm.com>
2016-12-09 17:29:31 +0530

    new program added

--- Hello.java
+++ Hello.java
@@ -2,7 +2,8 @@ class Hello {
 
 public static void main(String args[]){
 for(int i=0;i<=10;i++){
-System.out.println((i+1)+">>***Welcome to Jenkins###>>>"+(i+1));
+System.out.println((i+1)+">>***Welcome to Jenkins 123###>>>"+(i+1));
 }
 }
 }
+
